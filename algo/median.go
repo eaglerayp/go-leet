e3bedebe52b12a34064f0b96f205d458d271c80e
eaglerayp <eagle_rayp@hotmail.com>
2017-04-03 22:26:19 +0800

    update: #5

--- algo/median.go
+++ algo/median.go
@@ -11,8 +11,22 @@ func isPalindrome(s string) bool {
 	return true
 }
 
+func isPalindromeIndex(s string, begin, end int) bool {
+	if begin < 0 {
+		return false
+	}
+	for begin < end {
+		if s[begin] != s[end] {
+			return false
+		}
+		begin++
+		end--
+	}
+	return true
+}
+
 func longestPalindrome(s string) (result string) {
-	if len(s) < 1 {
+	if len(s) < 2 {
 		return s
 	}
 
@@ -23,15 +37,26 @@ func longestPalindrome(s string) (result string) {
 	// traverse the string
 	for i := 1; i < n; i++ {
 		// challenge the longest case
-		for nowL := i + 1; nowL > mL; nowL-- {
-			if subS := s[i-nowL+1 : i+1]; isPalindrome(subS) {
-				mL = nowL
-				result = subS
-				continue
-			}
+
+		// version 1 but slower
+		// for nowL := i + 1; nowL > mL; nowL-- {
+		// 	if subS := s[i-nowL+1 : i+1]; isPalindrome(subS) {
+		// 		mL = nowL
+		// 		result = subS
+		// 		continue
+		// 	}
+		// }
+
+		// version two, each i only compute twice
+		if isPalindromeIndex(s, i-mL, i) {
+			// max length +1 case
+			result = s[i-mL : i+1]
+			mL = mL + 1
+		} else if isPalindromeIndex(s, i-mL-1, i) {
+			// max length +2 case
+			result = s[i-mL-1 : i+1]
+			mL = mL + 2
 		}
 	}
-	// fmt.Println("S:", s)
-	// fmt.Println("result:", result)
 	return
 }
