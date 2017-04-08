976202adcbe9df0bb2d84c6d7b74979193621806
eaglerayp <eagle_rayp@hotmail.com>
2017-04-08 12:46:39 +0800

    feature: Add #11 maxArea

--- algo/median.go
+++ algo/median.go
@@ -137,3 +137,39 @@ func myAtoi(str string) int {
 
 	return result * sign
 }
+
+func maxArea(height []int) int {
+	max := 0
+	n := len(height)
+	if n < 2 {
+		return max
+	}
+
+	// first O(n) version
+
+	// for i, v := range height {
+	// 	width, j := 0, i
+	// 	for j < n {
+	// 		h := minInt(v, height[j])
+	// 		area := h * width
+	// 		max = maxInt(max, area)
+	// 		width++
+	// 		j = i + width
+	// 	}
+	// }
+
+	// O(logn) version
+	i, j := 0, n-1
+	for i < j {
+		h := minInt(height[i], height[j])
+		max = maxInt(max, (j-i)*h)
+		for height[i] <= h && i < j {
+			i++
+		}
+		for height[j] <= h && i < j {
+			j--
+		}
+	}
+
+	return max
+}
--- algo/median_test.go
+++ algo/median_test.go
@@ -106,3 +106,18 @@ func TestMyAtoi(t *testing.T) {
 	}
 
 }
+
+func TestMaxArea(t *testing.T) {
+	if maxArea([]int{1, 2, 3, 4, 5}) != 6 {
+		fmt.Println("T1")
+		t.Fail()
+	}
+	if maxArea([]int{1}) != 0 {
+		fmt.Println("T2")
+		t.Fail()
+	}
+	if maxArea([]int{3, 3}) != 3 {
+		fmt.Println("T3")
+		t.Fail()
+	}
+}
