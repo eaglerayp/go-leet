6b03994fa69b47ab422d75bc600f09d547edfe9f
eaglerayp <eagle_rayp@hotmail.com>
2017-04-08 23:32:04 +0800

    feature: Add #12 integer to roman

--- algo/median_test.go
+++ algo/median_test.go
@@ -121,3 +121,18 @@ func TestMaxArea(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestIntToRoman(t *testing.T) {
+	if intToRoman(3999) != "MMMCMXCIX" {
+		fmt.Println("T1")
+		t.Fail()
+	}
+	if intToRoman(100) != "C" {
+		fmt.Println("T2")
+		t.Fail()
+	}
+	if intToRoman(499) != "CDXCIX" {
+		fmt.Println("T3")
+		t.Fail()
+	}
+}
