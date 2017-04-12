785886cbead1cb68689c1d3dbeaf317f90480002
eaglerayp <eagle_rayp@hotmail.com>
2017-04-12 17:43:48 +0800

    feature: Add #14 LongestCommonPrefix

--- algo/easy_test.go
+++ algo/easy_test.go
@@ -139,3 +139,19 @@ func TestRomanToInt(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestLongestCommonPrefix(t *testing.T) {
+	if longestCommonPrefix([]string{"abc", "ab", "abcd"}) != "ab" {
+		fmt.Println("T1")
+		t.Fail()
+	}
+	if longestCommonPrefix([]string{"abc"}) != "abc" {
+		fmt.Println("T2")
+		t.Fail()
+	}
+
+	if longestCommonPrefix([]string{"abcdef", "bc"}) != "" {
+		fmt.Println("T3")
+		t.Fail()
+	}
+}
